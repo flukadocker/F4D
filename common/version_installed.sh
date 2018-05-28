1181e5a9a00ce66fc5d139bd4513d73e5f5edb53
David Horvath <github@horvathd.eu>
2018-05-28 13:29:58 +0200

    Update link to git repository on the welcome screen

--- common/version_installed.sh
+++ common/version_installed.sh
@@ -6,6 +6,6 @@ fluka_installed=$(grep "version" $FLUPRO/Version.tag | awk -F":" '{print $2}' | 
 flair_installed=$(dnf list installed flair | grep flair | awk '{print $2}')
 
 echo "Fluka version: $fluka_installed, flair version: $flair_installed"
-echo "More information at www.fluka.org and <github link here>"
+echo "More information at www.fluka.org and https://github.com/flukadocker/F4D"
 
 cat /etc/profile.d/disclaimer
